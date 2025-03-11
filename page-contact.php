3caef395736e8a55daa9e232ff3c6f41fde53600
kevin chen <yha0971@gmail.com>
2025-03-11 14:16:37 +0800

    feat: 更新中文版地圖

--- page-contact.php
+++ page-contact.php
@@ -23,9 +23,11 @@ get_header();
                 </div>
           </div>
     	  <div class="row no-gutters">
-    	  		<div id="map" class="col-12">
-    	  		      <google-map :id="'contact'"></google-map>
-    	  		</div>
+            <div id="map" class="col-12">
+                  <div class="ratio ratio-21x9">
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1481.066722192679!2d121.53829498606149!3d25.0396016440585!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abd64af3e50f%3A0xb1c139066873baa1!2z6Ie654Gj55W25Luj5paH5YyW5a-m6amX5aC0IEMtTEFC!5e0!3m2!1szh-TW!2stw!4v1737684695747!5m2!1szh-TW!2stw" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                  </div> <!-- .ratio -->
+            </div>
     	  </div>
     	  <div class="row no-gutters">
                 <div class="col-md-6 row no-gutters zh-project">
@@ -62,3 +64,24 @@ get_header();
 <?php 
 get_footer();
 ?>
+<style>
+.ratio-21x9 {
+    --bs-aspect-ratio: 42.8571428571%;
+}
+.ratio {
+    position: relative;
+    width: 100%;
+}
+.ratio>* {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+.ratio::before {
+    display: block;
+    padding-top: var(--bs-aspect-ratio);
+    content: "";
+}
+</style>
